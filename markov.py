e48bb280533c39b64948e516e3568f842f45acc0
Spencer Kee <spencerkee7@gmail.com>
2016-09-05 10:41:42 -0400

    Fixed readability and added documentation. Currently nonfunctional.

--- markov.py
+++ markov.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python
 import random
 
 def file_to_words(filename):
     '''
-    Converts a text file into a list of word strings.
+    Converts a text file into a list of words.
+
+    It applies the method split() on each line with whitespace as the separator.
+
+    EXAMPLE:
+    A file containing "One morning, when Gregor Samsa woke" would be converted
+    to ['One', 'morning,', 'when', 'Gregor', 'Samsa', 'woke']
     '''
     words = []
     with open(filename,'r') as f:
@@ -11,22 +18,43 @@ def file_to_words(filename):
                words.append(word)
     return words
 
-def ngrams(num_key_words, word_list):
+def words_to_ngrams(n, word_list):
     '''
-    Iterates through the list of word strings and returns all groups of
-    num_key_words + 1 strings in a list.
+    Converts a list of words into a list of ngrams (a sequence of n words)
+
+    EXAMPLE:
+    ['One', 'morning,', 'when', 'Gregor', 'Samsa', 'woke'] 
+    would convert to:
+    [['One', 'morning,', 'when'], 
+    ['morning,', 'when', 'Gregor'], 
+    ['when', 'Gregor', 'Samsa'], 
+    ['Gregor', 'Samsa', 'woke']]
+
     '''
-    for i in range(len(word_list)-num_key_words):
-        yield word_list[i:i+num_key_words+1]
+    ngrams = []
+    for i in range(len(words)-n+1):
+        ngrams.append(words[i:i+n])
+    return ngrams
 
 def make_database(word_list, num_key_words):
     '''
-    Returns a dictionary where the keys are all num_key_words length
-    adjacent string sequences in the word_list and the values are
-    all words (including duplicates) that imediately follow the key.
+    Takes in a list of words and creates a dictionary where the keys are every ngram and the 
+    values are a list of every possible word that follows them (including duplicates).
+
+    The keys are tuples so they will work in Python dictionaries. If ngrams occur multiple times
+    in the word list, then they are appended to the list in the value, e.g. if the key size is 1
+    and 'Gregor Samsa' is in the text 3 times, the dictionary will look like {('Gregor'):['Samsa','Samsa','Samsa']}
+
+    EXAMPLE:
+    With a num_key_words of 2 and the word_list as ['One', 'morning,', 'when', 'Gregor', 'Samsa', 'woke'],
+    the function produces:
+    {('One', 'morning,'): ['when'],
+    ('morning,', 'when'): ['Gregor'],
+    ('when', 'Gregor'): ['Samsa'],
+    ('Gregor', 'Samsa'): ['woke']}
     '''
     database = {}
-    for ngram in ngrams(num_key_words=num_key_words, word_list=word_list):
+    for ngram in words_to_ngrams(n=num_key_words+1, word_list=word_list):
         key = tuple(ngram[0:len(ngram)-1])
         try:
             database[key] = database[key] + [ngram[-1]]
@@ -38,12 +66,31 @@ def find_start_keys(database):
     '''
     Returns keys in a database where the first letter of the
     first word is uppercase.
+
+    This is used so that the generated sentences begin more realistically.
+
+    EXAMPLE: 
+    For the dictionary
+
+    {('One', 'morning,'): ['when'],
+    ('morning,', 'when'): ['Gregor'],
+    ('when', 'Gregor'): ['Samsa'],
+    ('Gregor', 'Samsa'): ['woke']}
+
+    the function returns
+
+    [('Gregor', 'Samsa'), ('One', 'morning,')]
     '''
     start_keys = []
     for key in database.keys():
         if key[0][0].isupper():
             start_keys.append(key)
     return start_keys
+
+def key_has_valid_punctuation():
+    for start_key in possible_start_keys:
+        if not any(punctuation in ''.join(start_key) for punctuation in ending_punctuation):
+            return list(start_key)
 
 def find_valid_seed(database, ending_punctuation):
     '''
@@ -84,7 +131,14 @@ def main(filename, num_key_words, chain_length, ending_punctuation=['.', '?', '!
     print (' '.join(gen_words))
 
 if __name__ == "__main__":
-    main (filename='metamorphosis', num_key_words=4, chain_length=100)
+    # main (filename='metamorphosis', num_key_words=4, chain_length=100)
+
+    # words = file_to_words('metamorphosis')
+    words = ['One', 'morning,', 'when', 'Gregor', 'Samsa', 'woke']
+    n=3
+    # x = (words_to_ngrams(3,words))
+    x = make_database(words, 2)
+    print (find_start_keys(x))
 
 '''
 Future Improvements:
