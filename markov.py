a65705a08447f7fa1fbd569346c234f39a0eaa54
Spencer Kee <spencerkee7@gmail.com>
2016-09-06 09:44:12 -0400

    Edited main docstring

--- markov.py
+++ markov.py
@@ -35,7 +35,7 @@ def words_to_ngrams(n, word_list):
         ngrams.append(word_list[i:i+n])
     return ngrams
 
-def make_database(word_list, num_key_words):
+def make_database(word_list, order):
     '''
     Takes in a list of words and creates a dictionary where the keys are every ngram and the 
     values are a list of every possible word that follows them (including duplicates).
@@ -45,7 +45,7 @@ def make_database(word_list, num_key_words):
     and 'Gregor Samsa' is in the text 3 times, the dictionary will look like {('Gregor'):['Samsa','Samsa','Samsa']}
 
     EXAMPLE:
-    With a num_key_words of 2 and the word_list as ['One', 'morning,', 'when', 'Gregor', 'Samsa', 'woke'],
+    With a order of 2 and the word_list as ['One', 'morning,', 'when', 'Gregor', 'Samsa', 'woke'],
     the function produces:
     {('One', 'morning,'): ['when'],
     ('morning,', 'when'): ['Gregor'],
@@ -53,7 +53,7 @@ def make_database(word_list, num_key_words):
     ('Gregor', 'Samsa'): ['woke']}
     '''
     database = {}
-    for ngram in words_to_ngrams(n=num_key_words+1, word_list=word_list):
+    for ngram in words_to_ngrams(n=order+1, word_list=word_list):
         key = tuple(ngram[0:len(ngram)-1])
         try:
             database[key] = database[key] + [ngram[-1]]
@@ -101,32 +101,32 @@ def find_start_seed(database, ending_punctuation=['.', '?', '!']):
             valid_start_keys.append(i)
     return random.choice(valid_start_keys)
 
-def main(filename, num_key_words, sentence_cutoff_length, ending_punctuation=['.', '?', '!']):
+def main(filename, order, sentence_cutoff_length, ending_punctuation=['.', '?', '!']):
     '''
-    Given a text file, create a database and generate a sentence beginning with
-    a random starting key. To generate the next word, check if the last
-    num_key_words are in the database. If so, randomly select the next word
-    from the words that follow the last num_key_words. If not, pick a word
-    at random from the words that follow the last word in the sentence. If
-    a word is generated that ends in ending_punctuation or the sentence
-    length reaches chain_length, break and return the sentence as a joined string.
-
-    Given a text file, this function will create a build a dictionary (database) which 
+    Given a text file, this function will create a dictionary (called the database) which 
     indicates how often a certain word follows another given word or words. It will then 
-    randomly generate a text by using this database.
+    randomly generate a sentence using this database by making each word in the sentence 
+    a random function of its predecessor(s). The order of the sentence indicates how many 
+    predecessors are taken into account. A sentence with order-1 is constructed by randomly
+    choosing a word that follows the previous one and might look like:
+
+    "Drops of greeting and her hurry she approached the unknown nourishment he thought."
+
+    A sentence with order-2 or order-3 would likely look more realistic (possibly because
+    depending on the text it can copy entire sentences)
 
     A generated sentence is returned if it reaches a word that ends in ending_punctuation or 
     it reaches sentence_cutoff_length. If the function can't proceed because the last few words 
     in the generated sentence don't occur anywhere in the text, it restarts the process. 
     '''
     words = file_to_words(filename)
-    database = make_database(words, num_key_words)
+    database = make_database(words, order)
     while True:
         return_sentence = True
         generated_words = list(find_start_seed(database, ending_punctuation))
         while (len(generated_words) < sentence_cutoff_length) and (generated_words[-1][-1] not in ending_punctuation):
             try:#possibly shorten
-                next_word = generated_words[-num_key_words:]
+                next_word = generated_words[-order:]
                 next_word = tuple(next_word)
                 next_word = random.choice(database[next_word])
             except KeyError:
@@ -137,7 +137,7 @@ def main(filename, num_key_words, sentence_cutoff_length, ending_punctuation=['.
             return ' '.join(generated_words)
 
 if __name__ == "__main__":
-    print(main (filename='metamorphosis', num_key_words=1, sentence_cutoff_length=100))
+    print(main (filename='metamorphosis', order=2, sentence_cutoff_length=100))
 
 '''
 Future Improvements (No guarantee of readability):
@@ -145,10 +145,10 @@ Future Improvements (No guarantee of readability):
 >Add command line support.
 >Convert functions to generators where useful for performance.
 >Possible problem with conversion to tuple, list(('cat')) returns ('c','a','t').
-Might cause issues with num_key_words=1, but it hasn't so far.
+Might cause issues with order=1, but it hasn't so far.
 >Consider switching from having tuple keys to string keys.
->Instead of essentially stepping with (num_key_words = 1) if a key
-isn't in the dictionary, construct databases for num_key_words ... 2.
+>Instead of essentially stepping with (order = 1) if a key
+isn't in the dictionary, construct databases for order ... 2.
 This means that if a key of length 4 isn't in the dictionary, it tries
 the last 3 words, then the last 2, and then just randomly selects from
 words that follow the last word, instead of going straight to the last step.
